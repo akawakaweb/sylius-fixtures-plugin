ec703320c385fed8a692ae1822569648a18c648b
Loïc Frémont <lc.fremont@gmail.com>
2023-05-02 11:29:39 +0200

    Allow Symfony 5.4

---
+++ src/Symfony/DependencyInjection/ShopFixturesPluginExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of ShopFixturesPlugin.
+ *
+ * (c) Akawaka
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Akawakaweb\ShopFixturesPlugin\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+final class ShopFixturesPluginExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new PhpFileLoader(
+            $container,
+            new FileLocator(dirname(__DIR__, 3) . '/config'),
+        );
+
+        $loader->load('services.php');
+    }
+}
---
+++ src/Symfony/ShopFixturesPlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of ShopFixturesPlugin.
+ *
+ * (c) Akawaka
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Akawakaweb\ShopFixturesPlugin\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class ShopFixturesPlugin extends Bundle
+{
+    public function getPath(): string
+    {
+        if (null === $this->path) {
+            $reflected = new \ReflectionObject($this);
+            // assume the modern directory structure by default
+            $this->path = \dirname($reflected->getFileName(), 3);
+        }
+
+        return $this->path;
+    }
+}
